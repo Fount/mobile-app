5821e3b77bbc8e7a7cf9f937e7cc26234dd210d4
linzp <sunandmoon001@163.com>
2016-06-12 15:54:28 +0800

    添加移动端调试器

--- template/app/index.js
+++ template/app/index.js
@@ -11,3 +11,7 @@ import Plugin from './lib/plugin'
 Vue.use(Plugin{{#if_eq weixinShare true}},{
     shareApi:'/activitymany/getWeixinInfo'{{/if_eq}}
 });
+
+//移动端调试器上线后去掉
+import eruda from 'eruda'
+eruda.init();
